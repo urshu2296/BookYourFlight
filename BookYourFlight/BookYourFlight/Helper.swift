4f771e0cdd16e6e2654c553c4a990c9a08aa8f48
Urvashi Tomar <157149784+urshu2296@users.noreply.github.com>
2024-02-16 10:37:15 +0530

    Add files via upload
    Added Helper and Model classes

---
+++ BookYourFlight/BookYourFlight/Helper.swift
@@ -0,0 +1,17 @@
+//
+//  Helper.swift
+//  Combine Practise
+//
+//  Created by MAC on 12/02/24.
+//
+
+import Foundation
+class Helper {
+    static func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM, EEE"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.string(from: date)
+    }
+
+}
---
+++ BookYourFlight/BookYourFlight/Model.swift
@@ -0,0 +1,34 @@
+//
+//  Model.swift
+//  Combine Practise
+//
+//  Created by MAC on 06/02/24.
+//
+import SwiftUI
+import Foundation
+
+class Model: ObservableObject {
+   
+    @Published var bookingVM = BookingModel(passengersCount: "2", payIn: "INR")
+    @Published var userTextField = ""
+    @Published var passwordTextField = ""
+    @Published var areYouGoingToSecondView = false
+
+}
+
+class BookingModel {
+    @Published var pickUpStation:String = "Patna"
+    @Published var dropStation:String = "Indore"
+    @Published var passengersCount:String = "0"
+    @Published var payIn:String = "INR"
+    @Published var promoCode:String = "CTY256"
+    @Published var pickdate:Date = Date()
+    @Published var returndate:Date = Date()
+    
+    init(passengersCount: String, payIn: String) {
+        self.passengersCount = passengersCount
+        self.payIn = payIn
+    }
+}
+
+
